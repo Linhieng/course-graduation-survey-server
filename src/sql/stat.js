cc95f87a5e8a641169181ca4e18012a885ae0d87
linhieng <790728740@qq.com>
2024-04-22 16:10:45 +0800

    添加统计收集的数据

--- src/sql/stat.js
+++ src/sql/stat.js
@@ -15,6 +15,10 @@ export const sqlGetCountStat = (userId) => useOneConn(async (conn) => {
         count_draft_survey: -1,
         count_publish_survey: -1,
         count_stop_survey: -1,
+
+        today_count_create_survey: -1,
+        today_count_publish_survey: -1,
+        today_count_answer_survey: -1,
     }
 
     sql = 'SELECT sum(stat.count_visit) as all_count_visit, sum(stat.count_answer) as all_count_answer ' +
@@ -39,6 +43,31 @@ export const sqlGetCountStat = (userId) => useOneConn(async (conn) => {
     sql = 'SELECT COUNT(*) as c FROM questionnaire WHERE creator_id = ? and is_draft=0 and is_valid=0;'
     result = await conn.execute(sql, values)
     res.count_stop_survey += result[0][0].c
+
+    // 根据问卷的创建时间，进行划分为今日的创建问卷数量
+    sql = 'SELECT COUNT(*) as c from questionnaire where creator_id = ? and created_at > CURDATE();'
+    values = [userId]
+    result = await conn.execute(sql, values)
+    res.today_count_create_survey = result[0][0].c
+
+    // 根据问卷的创建时间，进行划分为今日的创建问卷数量
+    sql = 'SELECT COUNT(*) as c from questionnaire where creator_id = ? and created_at > CURDATE()' +
+        ' and is_draft=0 and is_deleted=0 and is_valid=1;'
+    values = [userId]
+    result = await conn.execute(sql, values)
+    res.today_count_publish_survey = result[0][0].c
+
+    // 获取问卷回答数量
+    sql = 'SELECT COUNT(a.id) as c ' +
+        'from questionnaire_answer as a ' +
+        '         join questionnaire as q ' +
+        'where q.creator_id = ? ' +
+        '  and a.questionnaire_id = q.id ' +
+        '  and a.created_at > CURDATE(); '
+    values = [userId]
+    result = await conn.execute(sql, values)
+    res.today_count_answer_survey = result[0][0].c
+
 
     return res
 })
